9ecc6592ebaebab947327721285106c2d32e4a9c
zemuuv <samuvarg@gmail.com>
2025-02-05 00:29:00 -0500

    funciones random js

--- scrips.js
+++ scrips.js
@@ -1,2 +1,10 @@
-var numero=Math.random()
-console.log(numero)
+var numero
+ function Numero_Azar(){
+    numero=Math.floor(Math.random()*500)
+    document.getElementById("binario").innerHTML = numero
+    console.log(numero)
+ }
+
+ function binario(){
+    console.log(numero)
+ }
