0f23a573009883fe9ea6e466c100ebbe1490ff6f
zemuuv <samuvarg@gmail.com>
2025-02-07 00:12:43 -0500

    script de decimal a binario

--- scrips.js
+++ scrips.js
@@ -6,5 +6,32 @@ var numero
  }
 
  function binario(){
-    console.log(numero)
+   console.log("holaa")
+    let digitos = contar_digitos()
+    let lista = Generar_Array(digitos) 
+    console.log(lista)
+ }
+
+ function Generar_Array(digitos){
+   let numeros=numero
+   let arreglo=[]
+   let d=digitos-1
+   for (let i = 0; i < digitos; i++) {            
+      let parte=Math.floor(numeros%2)
+      numeros=Math.floor(numeros/2)
+      arreglo[d] = parte
+      d--
+   }
+   return arreglo
+ }
+
+ function contar_digitos(){
+   let numero1=numero
+   let digitos=0
+   while (numero1!=0){
+      numero1=Math.floor(numero1/2)
+      digitos++
+   }
+   console.log(digitos)
+   return digitos
  }
